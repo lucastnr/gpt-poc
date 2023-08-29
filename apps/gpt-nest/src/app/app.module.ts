9f5499178dd2b6556030d32a0e87ccd1ae602f55
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-08-29 00:11:30 -0300

    Move nest project to nx workspace

---
+++ apps/gpt-nest/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { GptService } from './gpt.service';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  })],
+  controllers: [AppController],
+  providers: [GptService],
+})
+export class AppModule {}
